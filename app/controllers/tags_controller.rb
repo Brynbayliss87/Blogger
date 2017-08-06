330c6cdc98ef6579fab6fc8f5c7e3b5873dcca1f
Bryn Bayliss <brynbayliss@hotmail.com>
2017-08-06 21:04:16 +0100

    added login functionality

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,11 @@
 class TagsController < ApplicationController
+  before_action :require_login, only: [:destroy]
+
   def show 
     @tag = Tag.find(params[:id])
   end
   
   def index
-    @tags = Tags.all
+    @tag = Tag.all
   end
 end
